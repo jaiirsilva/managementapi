bacf772408cc6600a2a6ba0bf146306436cefef0
Jair da Silva <jaiir.silva@live.com>
2021-06-02 19:31:19 -0300

    Terceiro commit

--- src/main/java/management/personapi/contoller/PersonController.java
+++ src/main/java/management/personapi/contoller/PersonController.java
@@ -2,12 +2,14 @@ package management.personapi.contoller;
 
 import management.personapi.dto.request.PersonDTO;
 import management.personapi.dto.response.MessageResponseDTO;
+import management.personapi.exception.PersonNotFoundException;
 import management.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -24,5 +26,15 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 }
--- src/main/java/management/personapi/service/PersonService.java
+++ src/main/java/management/personapi/service/PersonService.java
@@ -3,10 +3,15 @@ package management.personapi.service;
 import management.personapi.dto.request.PersonDTO;
 import management.personapi.dto.response.MessageResponseDTO;
 import management.personapi.entity.Person;
+import management.personapi.exception.PersonNotFoundException;
 import management.personapi.mapper.PersonMapper;
 import management.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -28,5 +33,19 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+
+        return personMapper.toDTO(person);
     }
 }
